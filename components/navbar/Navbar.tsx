0a774778ed07050aebac3fcfa87deacf5aaf32d6
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2025-04-15 20:20:27 -0600

    S2- Next/Link

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,3 +1,12 @@
+import Link from "next/link";
+import { HomeIcon } from "@primer/octicons-react";
+
+const navItems = [
+  { name: "About", href: "/about" },
+  { name: "Pricing", href: "/pricing" },
+  { name: "Contact", href: "/contact" },
+];
+
 const temporalAsync = () => {
   return new Promise((resolve) => {
     setTimeout(() => {
@@ -12,12 +21,16 @@ export const Navbar = async () => {
   await temporalAsync();
   return (
     <nav className="flex bg-blue-800 opacity-65 p-2 m-2 rounded text-white">
+      <Link className="flex items-center mr-3" href="/">
+        <HomeIcon className="mr-2"></HomeIcon>
         <span>Home</span>
+      </Link>
 
-        <div className="flex flex-1"></div>
-        <a className="mr-3" href="/about">About</a>
-        <a className="mr-3" href="/pricing">Princing</a>
-        <a className="mr-3" href="/contact">Contact</a>
+      <div className="flex flex-1"></div>
+
+      {navItems.map( navItem => (
+        <Link key={navItem.href} className="mr-3" href={ navItem.href }>{ navItem.name }</Link>
+      ))}
     </nav>
   )
 }
